713165285105977d00f4a15cd594e4b95303e963
WooJJam <111514410+WooJJam@users.noreply.github.com>
2025-02-04 13:04:50 +0900

    Test PR (#11)
    * commit 1
    
    * commit 2

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -6,12 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping
 public class TestController {
 
 	@GetMapping
 	public ResponseEntity test() {
+		log.info("commit 1");
+		log.info("commit 2");
 		return ResponseEntity.ok("Success!");
 	}
 }
