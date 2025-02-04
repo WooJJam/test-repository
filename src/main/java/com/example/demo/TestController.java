d8e630f665230127a2e5b699dcf76e5fb8655032
WooJJam <111514410+WooJJam@users.noreply.github.com>
2025-02-04 13:02:21 +0900

    commit 1

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -6,12 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping
 public class TestController {
 
 	@GetMapping
 	public ResponseEntity test() {
+		log.info("commit 1");
 		return ResponseEntity.ok("Success!");
 	}
 }
