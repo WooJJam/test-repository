c1dfcb24bce331970b5a8f44071495298c5e60b2
WooJJam <111514410+WooJJam@users.noreply.github.com>
2025-02-04 13:56:47 +0900

    test pr feature 4 (#14)
    * commit 8
    
    * commit 9
    
    * commit 10

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -6,12 +6,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping
 public class TestController {
 
-	@GetMapping
 	public ResponseEntity test() {
+		log.info("commit 1");
+		log.info("commit 2");
+		log.info("commit 3");
+		log.info("commit 4");
+		log.info("commit 5");
+		return ResponseEntity.ok("Success!");
+	}
+
+	public ResponseEntity test2() {
+		log.info("commit 6");
+		log.info("commit 7");
+		log.info("commit 8");
+		log.info("commit 9");
+		log.info("commit 10");
 		return ResponseEntity.ok("Success!");
 	}
 }
